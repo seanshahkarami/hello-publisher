ba583d5786edbd7168c9e907bd930872ec73cc47
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-06-22 13:40:27 -0500

    Added worker. Changed default user nameto hello-

--- hello-worker.py
+++ hello-worker.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 from waggle import beehive
-import time
 
 config = beehive.ClientConfig(
     host='10.10.10.5',
     port=23181,
     vhost='testing',
-    node='0000020000000000',
-    username='hello-plugin',
+    username='hello-worker',
     password='waggle',
     cacert='/Users/Sean/github/hello-publisher/cacert.pem',
     cert='/Users/Sean/github/hello-publisher/cert.pem',
     key='/Users/Sean/github/hello-publisher/key.pem')
 
-client = beehive.PluginClient(
-    name='hello:1',
-    config=config)
 
-while True:
-    client.publish('greeting', 'hello world', exchange='plugins-in')
-    time.sleep(1)
+def callback(data):
+    print(data)
+
+    # ...transform data...
+
+    return result
+
+
+client = beehive.WorkerClient(
+    name='hello:1',
+    config=config,
+    callback=callback)
+
+client.start_working()
